239d0a58c47bc0c9e3209313e587b8771d94d6a5
ernane <ernanecarvalho89@gmail.com>
2022-03-05 16:04:55 -0300

    adicionando o estado da aplicação

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
-
+import { useState } from "react";
+import ColorBox from './components/ColorBox';
 
 function App() {
+  const [red, setRed] = useState(0);
+  const [green, setGreen] = useState(0);
+  const [blue, setBlue] = useState(0);
   return (
-    <div className="App">
-      ola mundao
-    </div>
+    <>
+    <ColorBox backgroundColor={`rgb(${red}, ${green}, ${blue})`} />
+      <h3>Red: {red}</h3>
+      <input
+        type="range"
+        min={0}
+        max={255}
+        value={red}
+        onChange={({ target }) => setRed(parseInt(target.value))}
+      />
+      <br />
+      <h3>Green: {green}</h3>
+      <input
+        type="range"
+        min={0}
+        max={255}
+        value={green}
+        onChange={({ target }) => setGreen(parseInt(target.value))}
+      />
+      <br />
+      <h3>Blue: {blue}</h3>
+      <input
+        type="range"
+        min={0}
+        max={255}
+        value={blue}
+        onChange={({ target }) => setBlue(parseInt(target.value))}
+      />
+    </>
   );
 }
 
